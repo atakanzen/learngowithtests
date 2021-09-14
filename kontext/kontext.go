95ec2ddd2da8779d7e304d43de686643efaa7aeb
Atakan Zengin <atakanzzengin@gmail.com>
2021-09-14 14:03:09 +0300

    [Context] Finish module
    - How to test a HTTP handler that has had the request cancelled by the client.
    - How to use context to manage cancellation.
    - How to write a function that accepts context and uses it to cancel itself by using goroutines, select and channels.
    - Follow Google's guidelines as to how to manage cancellation by propagating request scoped context through your call-stack.
    - How to roll your own spy for http.ResponseWriter if you need it.

---
+++ kontext/kontext.go
@@ -0,0 +1,23 @@
+package kontext
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return // Log error
+		}
+
+		fmt.Fprint(w, data)
+	}
+}
